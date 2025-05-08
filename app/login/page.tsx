df64c5c956cd442cd46711300b935ac45f29c7e7
giep213 <gavinadlan@gmail.com>
2025-05-08 13:51:15 +0700

    add togglePasswordVisibility

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
+import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
 import { FcGoogle } from "react-icons/fc";
+import { FaEye, FaEyeSlash } from "react-icons/fa";
 
 export default function LoginPage() {
+  const [showPassword, setShowPassword] = useState(false);
+  const togglePasswordVisibility = () => setShowPassword((prev) => !prev);
+
   return (
     <div className="min-h-screen flex">
       {/* Left Decorative Panel */}
@@ -64,13 +69,22 @@ export default function LoginPage() {
                 <Label htmlFor="password" className="text-[rgb(39,68,124)]">
                   Password
                 </Label>
-                <Input
-                  id="password"
-                  type="password"
-                  placeholder="Password"
-                  className="mt-1 border-[rgb(39,68,124)] focus:ring-2 focus:ring-[rgb(73,163,90)]"
-                  required
-                />
+                <div className="relative">
+                  <Input
+                    id="password"
+                    type={showPassword ? "text" : "password"}
+                    placeholder="Password"
+                    className="mt-1 border-[rgb(39,68,124)] pr-10 focus:ring-2 focus:ring-[rgb(73,163,90)]"
+                    required
+                  />
+                  <button
+                    type="button"
+                    onClick={togglePasswordVisibility}
+                    className="absolute right-3 top-1/2 -translate-y-1/2 text-gray-500 hover:text-gray-700"
+                  >
+                    {showPassword ? <FaEyeSlash /> : <FaEye />}
+                  </button>
+                </div>
               </div>
 
               <div className="flex items-center justify-between">

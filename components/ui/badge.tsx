fc798433e763bab3a5ad02509a01cf80957224fe
giep213 <gavinadlan@gmail.com>
2025-05-27 12:00:46 +0700

    Modifie badge

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -1,6 +1,5 @@
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
-
 import { cn } from "@/utils/utils";
 
 const badgeVariants = cva(
@@ -15,6 +14,10 @@ const badgeVariants = cva(
         destructive:
           "border-transparent bg-destructive text-destructive-foreground hover:bg-destructive/80",
         outline: "text-foreground",
+        success:
+          "border-transparent bg-green-500 text-white hover:bg-green-500/80", // Tambahan
+        warning:
+          "border-transparent bg-yellow-500 text-white hover:bg-yellow-500/80", // Tambahan
       },
     },
     defaultVariants: {

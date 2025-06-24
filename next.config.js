af52a07baf555e94cb8c8dbe73a4331208989638
giep213 <gavinadlan@gmail.com>
2025-06-24 22:57:46 +0700

    Instal yaml, Swagger, raw loader

--- next.config.js
+++ next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.ya?ml$/,
+      use: "raw-loader",
+    });
+    return config;
+  },
 };
 
 module.exports = nextConfig;
